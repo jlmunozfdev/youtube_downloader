d1ecd4364ae9c0b5e702bf715c248c502362bd46
jlmunozfdev <jlmunozf.dev@gmail.com>
2024-06-04 22:18:26 -0500

    Update settings, index, _base, nav y footer

--- downloader/urls.py
+++ downloader/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns #For Deploy Render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns() # For Deploy Render
